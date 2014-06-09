8a640e8b5c478dc98aa69f154efdf497b88478b1
drp2pp <drp2pp@naver.com>
2014-06-09 16:24:10 +0900

    system Language 가져오기

--- src/com/r10m/gogoong/SettingActivity.java
+++ src/com/r10m/gogoong/SettingActivity.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.Locale;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -104,8 +105,11 @@ public class SettingActivity extends PreferenceActivity {
 		// 3. \data\data\패키지이름\shared_prefs\패키지이름_preferences.xml 생성
 		// 4. 이 후 Preference에 변경 사항이 생기면 파일에 자동 저장
 		
+		Locale systemLocale = getResources().getConfiguration().locale;
+	    String strLanguage = systemLocale.getLanguage();
+		
 		mainPreference = PreferenceManager.getDefaultSharedPreferences(this);
-		setLocale(mainPreference.getString("LanguageList", "ko"));
+		setLocale(mainPreference.getString("LanguageList", strLanguage));
 		setContentView(R.layout.setting);
 		addPreferencesFromResource(R.xml.settings);
 		
