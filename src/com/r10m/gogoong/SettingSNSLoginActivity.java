7c16b09d08fe5c2bd298830b32d35dae219ad34d
JangHeeJun <fpwldh@naver.com>
2014-06-09 15:56:20 +0900

    설정창 페북,트윗 연동

---
+++ src/com/r10m/gogoong/SettingSNSLoginActivity.java
@@ -0,0 +1,172 @@
+package com.r10m.gogoong;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.facebook.Session;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SettingSNSLoginActivity extends Activity {
+	ImageView profileImg;
+	TextView name;
+	Button logout;
+	Intent intent;
+	String target;
+	
+	Bitmap profileBitmap;
+	private ProgressBar progBar;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.setting_sns);
+		
+		// 전 Activity에서 data 받기
+		intent = getIntent();
+		target = intent.getExtras().getString("target");
+		
+		//프로필 사진
+		profileImg = (ImageView)findViewById(R.id.imageView_profile);
+		//본인 아이디
+		name = (TextView)findViewById(R.id.textView_profile);
+		
+		// progress bar
+		progBar = (ProgressBar)findViewById(R.id.progressBar_setting_sns);
+
+		
+		new ProfileImageTask().execute();
+		
+		
+		logout = (Button)findViewById(R.id.btn_logout);
+		logout.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				logout();
+				finish();
+			}
+		});	
+	}
+	
+	class ProfileImageTask extends AsyncTask<Void, Void, Bitmap>{
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			progBar.setVisibility(View.VISIBLE);
+		}
+		
+		@Override
+		protected Bitmap doInBackground(Void... params) {
+			URL ProfileImageUrl = null;
+    		profileBitmap = null;
+    		HttpURLConnection con = null;
+    		BufferedInputStream bis=null;
+            try{
+               	ProfileImageUrl = new URL(intent.getExtras().getString("url"));
+                
+                con = (HttpURLConnection)ProfileImageUrl.openConnection();
+                con.setDoInput(true);
+                con.setRequestMethod("GET");
+                con.connect();
+                bis = new BufferedInputStream(con.getInputStream());
+                profileBitmap = BitmapFactory.decodeStream(bis);
+                Log.e("facebookimage", profileBitmap.toString());
+                 
+            }catch (MalformedURLException e){
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            } finally{
+    			try {
+    				if(bis!=null) bis.close();
+    				if(con!=null) con.disconnect();
+    			} catch (IOException e) {
+    				e.printStackTrace();
+    			}
+    		}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Bitmap result) {
+			super.onPostExecute(result);
+			progBar.setVisibility(View.GONE);
+			
+			// 각 뷰에 적용
+			profileImg.setImageBitmap(profileBitmap);
+			name.setText(intent.getExtras().getString("name"));
+			
+		}
+
+	}
+	
+	
+	// logout
+	private void logout(){
+		if (target.equals("facebook")){
+            Session session = Session.getActiveSession();
+            if(session!=null){
+            	session.closeAndClearTokenInformation();
+            }
+        }else if (target.equals("twitter")){
+        	BasicInfo.TwitLogin = false;
+        	BasicInfo.TWIT_KEY_TOKEN = "";
+        	BasicInfo.TWIT_KEY_TOKEN_SECRET = "";
+        	BasicInfo.TwitScreenName = "";
+        	twitSaveProperties();
+        }
+    	Toast.makeText(this, getString(R.string.logout_setting), Toast.LENGTH_SHORT).show();
+	}
+	
+	private void twitSaveProperties() {
+		SharedPreferences pref = getSharedPreferences("TWIT", MODE_PRIVATE);
+		SharedPreferences.Editor editor = pref.edit();
+
+		editor.putBoolean("TwitLogin", BasicInfo.TwitLogin);
+		editor.putString("TWIT_KEY_TOKEN", BasicInfo.TWIT_KEY_TOKEN);
+		editor.putString("TWIT_KEY_TOKEN_SECRET", BasicInfo.TWIT_KEY_TOKEN_SECRET);
+		editor.putString("TwitScreenName", BasicInfo.TwitScreenName);
+
+		editor.commit();
+	}
+	
+	@Override
+	public void finish() {
+		super.finish();
+		overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+	}	
+}
